654c4ec810c19db2052d01fbb8dbf1a35780b280
itzish1999 <ismaeel.butt19@gmail.com>
2022-11-17 14:35:43 -0500

    feat(Pokemon Data): Fetched Data From PokemonApi and Presented the Data Once Button is Clicked.

--- client-api-test/src/App.js
+++ client-api-test/src/App.js
@@ -3,19 +3,45 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([]);
   const [searchInput, setSearchInput] = useState("");
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemonData, setPokemonData] = useState({
+    name: "",
+    img: "",
+    ability: "",
+    experience: "",
+    forms: ""
+  });
+
 
   useEffect(() => {
     axios.get("https://pokeapi.co/api/v2/pokemon")
       .then(res => {
-        console.log("Getting from Server ::::", res.data)
-        setData(res.data)
+        // console.log("Getting from Server ::::", res.data)
+        setPokemon(res.data)
+        console.log("DATA :::: ", res.data)
       }).catch(err => console.log(err))
   }, [])
 
+  function getPokemonData(data) {
+    axios.get(data.url).then(res => {
+      setPokemonData({
+        name: res.data.name,
+        img: res.data.sprites.front_default,
+        ability: res.data.abilities[0].ability.name,
+        experience: res.data.base_experience,
+        forms: res.data.forms[0].name
+      });
+      setPokemonChosen(true)
+      console.log(res.data)
+    }).catch(err => {
+      console.log("Error::: ", err);
+    })
+  }
+
   let pokeMap = {};
-  const arr = data.results && data.results.length > 0 && data.results.filter((data => {
+  const arr = pokemon.results && pokemon.results.length > 0 && pokemon.results.filter((data => {
     if (searchInput === "") {
       return data;
     } else if (data.name.toLowerCase().includes(searchInput.toLowerCase())) {
@@ -24,42 +50,57 @@ function App() {
       pokeMap['url'] = data.url;
       return data;
     }
+    console.log("DATA :::: ", data);
   }))
     .map((data, index) => {
       return (
         <div>
           <th style={{ paddingRight: '5px' }}>
-            <tr>
-              <td>{data.name}</td>
-            </tr>
+            <td><button onClick={() => getPokemonData(data)}>
+              {data.name}
+            </button></td>
+            <div className="Display">{!pokemonChosen ? (
+              <h1>Please Choose a Pokemon</h1>
+            ) : (
+              <>
+                <h1>{pokemonData.name}</h1>
+                <img src={pokemonData.img} />
+                <h3>Species: {pokemonData.forms}</h3>
+                <h3>ability: {pokemonData.ability}</h3>
+                <h4>Experience: {pokemonData.experience}</h4>
+              </>
+            )}
+            </div>
           </th>
-          <th>
-            <tr>
-              <td>{data.url}</td>
-            </tr>
-          </th>
+          <td></td>
         </div>
       )
     })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   // console.log("The KEYS are ::::", Object.keys(arr));
   // console.log("The VALUES are ::::", Object.values(arr));
 
   return (
     <div className="App">
-      <h1>Here Are Pokemon </h1>
-      <input type="text"
-        placeholder="Enter Pokemon Name"
-        onChange={e => {
-          setSearchInput(e.target.value)
-        }}
-      />
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Url</th>
-        </tr>
-      </table>
-      {arr}
+      <form onSubmit={handleSubmit}>
+        <h1>Here Are Pokemon </h1>
+        <input type="text"
+          placeholder="Enter Pokemon Name"
+          value={searchInput}
+          onChange={e => {
+            setSearchInput(e.target.value)
+          }}
+        />
+        <table>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </table>
+        {arr}
+      </form>
     </div>
   );
 }
