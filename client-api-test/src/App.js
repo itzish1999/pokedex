2d9ba402eb1151d2a372ba7a47658ecf9a3ba6b4
itzish1999 <ismaeel.butt19@gmail.com>
2022-11-21 14:32:54 -0500

    feat(Fixed Naming Conventions): Clarified useState by changing their names to match use

--- client-api-test/src/App.js
+++ client-api-test/src/App.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
-  const [pokemon, setPokemon] = useState([]); //fetches all pokemon
-  const [pokemonChosen, setPokemonChosen] = useState(false);// clicked
+  const [fetchPokemon, setFetchPokemon] = useState([]); //fetches all pokemon
+  const [isPokemonChosen, setIsPokemonChosen] = useState(false);// clicked
   const [pokemonData, setPokemonData] = useState({
     name: "",
     img: "",
@@ -19,7 +19,7 @@ function App() {
     axios.get("https://pokeapi.co/api/v2/pokemon")
       .then(res => {
         // console.log("Getting from Server ::::", res.data)
-        setPokemon(res.data)
+        setFetchPokemon(res.data)
         console.log("DATA :::: ", res.data)
       }).catch(err => console.log(err))
   }, [])
@@ -33,7 +33,7 @@ function App() {
         experience: res.data.base_experience,
         forms: res.data.forms[0].name
       });
-      setPokemonChosen(true)
+      setIsPokemonChosen(true)
       console.log(res.data)
     }).catch(err => {
       console.log("Error::: ", err);
@@ -41,7 +41,7 @@ function App() {
   }
 
   let pokeMap = {};
-  const arr = pokemon?.results?.length > 0 && pokemon.results.filter((data => {
+  const arr = fetchPokemon?.results?.length > 0 && fetchPokemon.results.filter((data => {
     if (searchInput === "") {
       return data;
     } else if (data.name.toLowerCase().includes(searchInput.toLowerCase())) {
@@ -59,7 +59,7 @@ function App() {
             <td><button onClick={() => getPokemonData(data)}>
               {data.name}
             </button></td>
-            <div className="Display">{pokemonChosen && pokemonData.name === data.name && (
+            <div className="Display">{isPokemonChosen && pokemonData.name === data.name && (
               <>
                 <h1>{pokemonData.name}</h1>
                 <img src={pokemonData.img} />
