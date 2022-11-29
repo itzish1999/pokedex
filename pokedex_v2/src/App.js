3ef4fe095e66a9227ddfaaeb36e13fd904c81901
itzish1999 <ismaeel.butt19@gmail.com>
2022-11-29 10:58:39 -0500

    feat(Pokemon Names): Used axios to get pokemon name and set the state

--- pokedex_v2/src/App.js
+++ pokedex_v2/src/App.js
@@ -1,9 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 import Card from "./components/Card";
 import Heading from "./components/Heading";
 import './App.css';
 
 
 function App() {
+  const [pokemonURL, setPokemonURL] = useState([]);
+
+  const fetchPokemonNames = async () => {
+    await axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then(res => {
+        setPokemonURL(res.data.results);
+      }).catch(err => console.log(err))
+  };
+
+
+  useEffect(() => {
+    fetchPokemonNames();
+  }, []);
+
+  console.log("IS THE STATE UPDATED:::: ", pokemonURL)
+  // we have 20 URLs we need to be able to go into each one. How can we do that?
+
+
+
+  // if (pokemonURL && pokemonURL.length > 0) {
+  //   axios.get(data.url)
+
+  // }
+
+
   return (
     <div className="App">
       <div className="heading">
