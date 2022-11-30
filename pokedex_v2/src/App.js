4cac82ac040b96d37eef8b8838f564a85e63446d
itzish1999 <ismaeel.butt19@gmail.com>
2022-11-30 10:17:19 -0500

    feat(Cleanup): Cleaned up the code

--- pokedex_v2/src/App.js
+++ pokedex_v2/src/App.js
@@ -11,18 +11,19 @@ function App() {
 
   function fetchPokemonDetails() {
     pokemonURLS.map(data => {
-      axios.get(data.url).then(res => {
-        console.log("Mapped into url to present pokemon details", res.data)
-        setPokemonDetails(res.data)
-      })
+      return (
+        axios.get(data.url).then(res => {
+          console.log("Mapped into url to present pokemon details", res.data)
+          setPokemonDetails(res.data)
+        }))
     })
   }
 
   function fetchPokemon() {
     axios.get("https://pokeapi.co/api/v2/pokemon")
       .then(res => {
-        console.log("Get pokemon names and urls", res.data)
-        setPokemonURLS(res.data.results, () => { console.log("hbh") })
+        console.log("Get pokemon names and urls", res.data.results)
+        setPokemonURLS(res.data.results, () => { })
       })
       .catch(err => console.log(err))
   };
@@ -38,14 +39,6 @@ function App() {
       fetchPokemonDetails();
     }
   }, [pokemonURLS]);
-
-  // console.log("IS THE STATE UPDATED:::: ", pokemonURL)
-  // We have 20 URLs we need to be able to go into each one. How can we do that?
-
-
-  /* If statement is just a check. See if the data is returned then .map over the data, 
-  axios into the url and then return data */
-
 
   return (
     <div className="App">
