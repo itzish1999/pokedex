f80f30a30ae8acfffd2ac5c26bb213516bd64bf6
itzish1999 <ismaeel.butt19@gmail.com>
2022-12-11 09:39:18 -0500

    feat(Insert Statement): Insert statement for putting data into db

--- backend/src/test/index.js
+++ backend/src/test/index.js
@@ -9,3 +9,34 @@ const client = new pg.Client({
     port: 5433,
     database: "pokemon"
 })
+
+await client.connect();
+
+try {
+    const result = await pd.getPokemonList({
+        limit: 20,
+    });
+    const arrPokemon = result.results;
+
+    for (let i = 0; i < arrPokemon.length; i++) {
+        const r = arrPokemon[i];
+        try {
+            const p = await pd.getPokemonByName(r.name)
+
+            const query = {
+                text: "INSERT INTO pokemon (name, form, image, experience) VALUES ($1, $2, $3, $4)",
+                values: [p.name, p.forms.name, p.sprites.front_default, p.base_experience],
+            };
+            await client.query(query);
+
+            process.stdout.write('.');
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+} catch (e) {
+    console.error(e);
+}
+
+await client.end();
