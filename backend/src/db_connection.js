bdebb7de9088d97c320ba885dc35b6ead0a19a80
itzish1999 <ismaeel.butt19@gmail.com>
2022-12-12 13:40:02 -0500

    feat(Fetch From DB): Successfully fetch get all request from database

---
+++ backend/src/db_connection.js
@@ -0,0 +1,5 @@
+const { Client } = require("pg");
+
+const db_client = new Client();
+
+module.exports = db_client
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,70 +1,17 @@
 const express = require('express');
-const axios = require('axios');
+const db_client = require('./db_connection.js');
+const { PORT } = require('../config');
 
 const app = express();
+db_client.connect();
 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-
-    next();
-});
-
-const getAllPokemon = async () => {
-    try {
-        const headers = {
-            headers: {
-                'Accept-Encoding': 'application/json',
-            }
-        };
-        const { data: response } = await axios.get('https://pokeapi.co/api/v2/pokemon', headers);
-        return response.results;
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-async function getAllPokemonDetails(pokemon) {
-    const headers = {
-        headers: {
-            'Accept-Encoding': 'application/json',
+app.get('/pokemon/getAll', (req, res) => {
+    db_client.query('SELECT * from pokemon', (err, result) => {
+        if (!err) {
+            res.send(result.rows)
+            db_client.end;
         }
-    };
-    const transformedPokemonArray = pokemon.map(async pokemonDetail => {
-        return await axios.get(pokemonDetail.url, headers).then(res => {
-
-            const { name, sprites: { front_default }, forms, abilities, moves, base_experience } = res.data;
-
-            // name , sprintes, forms, abilities, moves, base_experience
-
-            //create table with above 6 columns
-            // create an insert sql statement as a sample
-            // fetch that sample inserted with a select * from tableName where name = sampleName
-            // learn how to alter table to add more columns or change primary key, what is primary key?
-
-
-            const test = {
-                name,
-                image: front_default,
-                form: forms[0].name,
-                abilities,
-                moves,
-                experience: base_experience
-            };
-            pokemonDetail = test;
-            return pokemonDetail;
-        }).catch(err => {
-            console.log(err);
-        })
     })
-    const resolvedPromises = await Promise.all(transformedPokemonArray)
-    return resolvedPromises
-}
-
-app.get('/pokemon/getAll', async (req, res) => {
-    const pokemon = await getAllPokemon();
-    const pokemonDetails = await getAllPokemonDetails(pokemon);
-    pokemonDetails.length > 0 ? res.send(pokemonDetails) : res.send("No Pokemon Found")
 });
 
 app.get('/', (req, res) => {
@@ -72,5 +19,4 @@ app.get('/', (req, res) => {
     console.log(res)
 })
 
-const { PORT } = require('../config');
 app.listen(PORT, () => console.log(`Node server is running on PORT ${PORT}`));
