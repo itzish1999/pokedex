4d421baa9047cf0e30c591eb0f49997b5b67740c
itzish1999 <ismaeel.butt19@gmail.com>
2023-01-10 17:48:36 -0500

    feat(Return Data from DB): Successfully returned all the data from the database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const db_client = require('./db_connection.js');
 const { PORT } = require('../config');
-const pokemonRoutes = require("./Routes/routes");
 
 const app = express();
-app.use(pokemonRoutes);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    next();
+});
+
+app.get('/pokemon/getAll', (req, res) => {
+    db_client.query('SELECT * from pokemon', (err, result) => {
+        if (!err) {
+            res.send(result.rows)
+            db_client.end;
+        }
+    })
+})
+
+db_client.connect();
+
+
 
 app.get('/', (req, res) => {
     res.send("Hello World")
